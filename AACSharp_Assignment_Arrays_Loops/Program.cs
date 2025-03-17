3ebbb0efd2174c66926edac0baf30f4dc8fc5fd7
celalkorucu <21010310079@ogrenci.bartin.edu.tr>
2025-03-17 11:27:55 +0300

    Add project files.

---
+++ AACSharp_Assignment_Arrays_Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] arrays = new int[5];
+    for(int i = 0; i < 5; i++)
+{
+    Console.Write(i+1 + ". Sayıyı giriniz : ");
+    int number = int.Parse(Console.ReadLine());
+    arrays[i] = number;
+
+}
+
+double sum = 0;
+    foreach(int number in arrays)
+{
+    sum += number;
+}
+
+Console.WriteLine("Ortalama Sonucu : "+sum/arrays.Length);
